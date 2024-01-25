00b7fdd59c99113b610317dd450dd7744c20443e
thomas choubrac <thomas.choubrac06@orange.fr>
2024-01-25 10:29:37 +0100

    Modification Assignments

--- model/assignment.js
+++ model/assignment.js
@@ -5,7 +5,11 @@ let AssignmentSchema = Schema({
     id: Number,
     dateDeRendu: Date,
     nom: String,
-    rendu: Boolean
+    rendu: Boolean,
+    auteur: String,
+    matiere: String,
+    note: Number,
+    remarques: String
 });
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
