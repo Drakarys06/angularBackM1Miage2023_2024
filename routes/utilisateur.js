1be03d3478ef35677ce7b86c614b1648feda1093
bl000451 <lena.bitch@etu.unice.fr>
2024-01-26 18:19:14 +0100

    fix

--- routes/utilisateur.js
+++ routes/utilisateur.js
@@ -4,10 +4,13 @@ let User = require('../model/utilisateur');
 // Récupérer un utilisateur par son login (GET)
 function getUser(req, res){
     let userId = req.params.id;
+    console.log(userId);
 
     User.findOne({id: userId}, (err, user) =>{
         if(err){res.send(err)}
+        console.log(`User: ${JSON.stringify(user)}`);
         res.json(user);
+
     })
 }
 module.exports = { getUser };
