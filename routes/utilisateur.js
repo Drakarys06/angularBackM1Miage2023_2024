03409cde2a6fad807a0c846046c7b1b7c2fe62e6
thomas choubrac <thomas.choubrac06@orange.fr>
2024-01-28 19:15:30 +0100

    GetUsers

--- routes/utilisateur.js
+++ routes/utilisateur.js
@@ -3,15 +3,24 @@ let User = require('../model/utilisateur');
 
 // Récupérer un utilisateur par son login (GET)
 function getUser(req, res){
-    let userLogin = req.params.login;
-    console.log(userLogin);
+    let userId = req.params.id;
+    console.log(userId);
 
-    User.findOne({login: userLogin}, (err, login) =>{
+    User.findOne({id: userId}, (err, user) =>{
         if(err){res.send(err)}
-        console.log(`User: ${JSON.stringify(login)}`);
-        res.json(login);
+        console.log(`User: ${JSON.stringify(user)}`);
+        res.json(user);
 
     })
 }
 
-module.exports = { getUser };
+function getUsers(req, res){
+    console.log("getUsers");
+    User.find({}, (err, users) => {
+        if(err){res.send(err)}
+        res.json(users);
+        console.log(`Users: ${JSON.stringify(users)}`);
+    })
+}
+
+module.exports = { getUser, getUsers};
